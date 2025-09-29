6860df72ae382ad3ef73429b5e9c919a2e41841c
Zachary Northrup <z-systems@hotmail.com>
2025-09-29 01:06:55 -0700

    Cache the result of checking for Quick Look and Seer Pro instead of checking repeatedly

--- src/Files.App/Services/PreviewPopupProviders/PreviewPopupService.cs
+++ src/Files.App/Services/PreviewPopupProviders/PreviewPopupService.cs
@@ -6,14 +6,21 @@ namespace Files.App.Services.PreviewPopupProviders
 	/// <inheritdoc cref="IPreviewPopupService"/>
 	internal sealed partial class PreviewPopupService : ObservableObject, IPreviewPopupService
 	{
+		bool hasDetectedProvider = false;
+		IPreviewPopupProvider? provider = null;
+
 		public async Task<IPreviewPopupProvider?> GetProviderAsync()
 		{
+			if (hasDetectedProvider)
+				return provider;
+
+			hasDetectedProvider = true;
 			if (await QuickLookProvider.Instance.DetectAvailability())
-				return await Task.FromResult<IPreviewPopupProvider>(QuickLookProvider.Instance);
+				return provider = await Task.FromResult<IPreviewPopupProvider>(QuickLookProvider.Instance);
 			if (await SeerProProvider.Instance.DetectAvailability())
-				return await Task.FromResult<IPreviewPopupProvider>(SeerProProvider.Instance);
+				return provider = await Task.FromResult<IPreviewPopupProvider>(SeerProProvider.Instance);
 			else
-				return null;
+				return provider = null;
 		}
 	}
 }
